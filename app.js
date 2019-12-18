1d1ecff52fb83bc3366f1798ed6fe5381e945db3
Matthew Knight <mattknight.sa@gmail.com>
2019-12-17 21:33:43 -0600

    update fs write

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
 const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
@@ -51,8 +52,88 @@ inquirer
         // console.log(title);
         const engineerOne = new Engineer(github, name, id, title);
         console.log(engineerOne.github);
+        buildProfile(engineerOne);
     })
 
+}
+
+function buildProfile(user) {
+    fs.writeFile('team.html',`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Employee Summary</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                        <div class="card" style="width: 18rem;">
+                                <div class="card-header">
+                                  ${user.name}
+                                </div>
+                                <ul class="list-group list-group-flush">
+                                  <li class="list-group-item">${user.github}</li>
+                                  <li class="list-group-item">${user.id}</li>
+                                  <li class="list-group-item">${user.title}</li>
+                                </ul>
+                        </div>
+                </div>
+                <div class="col-md-6">
+                        <div class="card" style="width: 18rem;">
+                                <div class="card-header">
+                                  Featured
+                                </div>
+                                <ul class="list-group list-group-flush">
+                                  <li class="list-group-item">Cras justo odio</li>
+                                  <li class="list-group-item">Dapibus ac facilisis in</li>
+                                  <li class="list-group-item">Vestibulum at eros</li>
+                                </ul>
+                        </div>
+                </div>
+                  
+            </div>
+            <div class="row">
+                    <div class="col-md-6">
+                            <div class="card" style="width: 18rem;">
+                                    <div class="card-header">
+                                      Featured
+                                    </div>
+                                    <ul class="list-group list-group-flush">
+                                      <li class="list-group-item">Cras justo odio</li>
+                                      <li class="list-group-item">Dapibus ac facilisis in</li>
+                                      <li class="list-group-item">Vestibulum at eros</li>
+                                    </ul>
+                            </div>
+                    </div>
+                    <div class="col-md-6">
+                            <div class="card" style="width: 18rem;">
+                                    <div class="card-header">
+                                      Featured
+                                    </div>
+                                    <ul class="list-group list-group-flush">
+                                      <li class="list-group-item">Cras justo odio</li>
+                                      <li class="list-group-item">Dapibus ac facilisis in</li>
+                                      <li class="list-group-item">Vestibulum at eros</li>
+                                    </ul>
+                            </div>
+                    </div>
+                      
+                </div>
+    
+              
+        </div>
+        
+    </body>
+    </html>`,
+    function(err) {
+      if (err) {
+          throw err;
+        }
+    })
 }
 
 
@@ -74,28 +155,7 @@ function determineEmployee() {
 }
 
 determineEmployee()
+// buildProfile()
 
-// function promptUser() {
-//     const questions = [{
-//     message: "What is your name?",   
-//     name: "name"},
-//     {
-//     message: "What is your id?",
-//     name: "id"},
-//     {
-//     message: "What is your title?",
-//     name: "title"
-//     }
-//     ]
 
-// inquirer
-//     .prompt(questions).then(({name,id,title}) => {
-//         // console.log(name);
-//         // console.log(id);
-//         // console.log(title);
-//         const employeeOne = new Employee(name, id, title);
-//         console.log(employeeOne.name);
-//     })
-
-// }
 
