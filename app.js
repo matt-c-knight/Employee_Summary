9d3ad9645f66af60d20d7f8fede483db74ca6c11
Matthew Knight <mattknight.sa@gmail.com>
2019-12-17 18:50:44 -0600

    adding to app.js

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
 const inquirer = require("inquirer");
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
 
-inquirer
-    .prompt([
-        {
+function determineEmployee() {
+    const question = [{
+        message: "What is your role?",
+        name: "role"
+    }]
+    inquirer
+    .prompt(question).then
+}
 
-        }
+// function promptUser() {
+//     const questions = [{
+//     message: "What is your name?",   
+//     name: "name"},
+//     {
+//     message: "What is your id?",
+//     name: "id"},
+//     {
+//     message: "What is your title?",
+//     name: "title"
+//     }
+//     ]
 
-    ]).then(answers => {
-        
-    })
+// inquirer
+//     .prompt(questions).then(({name,id,title}) => {
+//         // console.log(name);
+//         // console.log(id);
+//         // console.log(title);
+//         const employeeOne = new Employee(name, id, title);
+//         console.log(employeeOne.name);
+//     })
+
+// }
+
+// promptUser()
